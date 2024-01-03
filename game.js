0f848c10f9bc1198783e708c502854afa9bf49f1
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-03 19:54:48 +0200

    Added round logic

--- game.js
+++ game.js
@@ -48,3 +48,33 @@ const getUserChoice = async () => {
 };
 
 const resetScores = () => ({ userScore: 0, computerScore: 0 });
+
+const determineRoundWinner = (userChoice, computerChoice) => {
+  const isUserWin = gameRules[userChoice].beats === computerChoice;
+  const isComputerWin = gameRules[computerChoice].beats === userChoice;
+  return isUserWin ? 'user' : isComputerWin ? 'computer' : 'tie';
+};
+
+const displayRoundResult = (winner, userChoice, computerChoice) => {
+  const messages = {
+    user: gameRules[userChoice].message,
+    computer: gameRules[computerChoice].message,
+    tie: 'The round is a draw!',
+  };
+  console.log(messages[winner]);
+};
+
+const updateScores = (winner, user, computer) => {
+  const newUserScore = user + (winner === 'user');
+  const newComputerScore = computer + (winner === 'computer');
+  return { userScore: newUserScore, computerScore: newComputerScore };
+};
+
+const playRound = async ({ userScore, computerScore }) => {
+  const userChoice = await getUserChoice();
+  const computerChoice = getComputerChoice();
+  const roundWinner = determineRoundWinner(userChoice, computerChoice);
+  displayRoundResult(roundWinner, userChoice, computerChoice);
+  const updatedScores = updateScores(roundWinner, userScore, computerScore);
+  return updatedScores;
+};
