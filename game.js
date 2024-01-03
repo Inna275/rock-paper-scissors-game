94f658da926d679fe5b25607078f7ecd1aa85bcf
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-03 17:42:37 +0200

    Added gameRules

--- game.js
+++ game.js
@@ -6,3 +6,18 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+const gameRules = {
+  rock: {
+    beats: 'scissors',
+    message: 'Rock crushes scissors',
+  },
+  paper: {
+    beats: 'rock',
+    message: 'Paper covers rock',
+  },
+  scissors: {
+    beats: 'paper',
+    message: 'Scissors cut paper',
+  },
+};
